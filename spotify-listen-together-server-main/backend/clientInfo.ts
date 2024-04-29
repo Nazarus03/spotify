636fa0e2d19accc0119ed79ee3b4197a22f465dd
Nazar <59307309+Nazarus03@users.noreply.github.com>
2024-04-29 22:19:07 +0300

    Update clientInfo.ts

--- spotify-listen-together-server-main/backend/clientInfo.ts
+++ spotify-listen-together-server-main/backend/clientInfo.ts
@@ -3,7 +3,7 @@ import { Socket } from "socket.io"
 export default class ClientInfo {
   public latency = 0
   public name = "Unnamed"
-  public isHost = True
+  public isHost = true
   public loggedIn = false
   public trackUri = ""
 
